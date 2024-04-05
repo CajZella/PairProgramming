96460e09afbf79fec3d5c421be9253330e4f6df9
CajZella <cajzella@gmail.com>
2024-04-05 23:00:21 +0800

    t3-2 xingneng

--- T3/t3-2-c/mancala_operator.c
+++ T3/t3-2-c/mancala_operator.c
@@ -9,8 +9,8 @@
 
 double dfs(int* next_move, int flag, int op, int *status, int depth, int init_score1, int init_score2, double fa) { // flag: 1 for player 1, 2 for player 2
     // op = 0: max, op = 1: min
-    double weights[4] = {0.5, 0.2, 0.5};
-    int depths = 8;
+    double weights[4] = {0.7, 0.0, 0.2, 0.2};
+    int depths = 7;
     int status_cpy[14];
     memcpy(status_cpy, status, sizeof(status_cpy));
     double res = op == 0 ? -1e9 : 1e9;
@@ -31,36 +31,37 @@ double dfs(int* next_move, int flag, int op, int *status, int depth, int init_sc
             sum2 += status[j + 7];
             sum1_ += status_cpy[j];
         }
+        int next_flag = (pos == 6 && flag == 1 || pos == 13 && flag == 2) ? flag : 3 - flag;
         if (depth + 1 == depths || sum1 == 0 || sum2 == 0) {
             // calculate the features
             double features[4];
             features[0] = status[6] - init_score1 - (status[13] - init_score2);
             features[1] = sum1 - sum2;
             features[2] = sum1_ - sum1 + status[6] - init_score1;
+            features[3] = next_flag == flag && flag == 1 ? 50 : (next_flag == flag && flag == 2 ? -50 : 0);
             // 归一化
             double sum = 0, target = 0;
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 4; j++)
                 features[j] = exp(features[j]), sum += features[j];
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 4; j++)
                 features[j] /= sum, target += features[j] * weights[j];
             if (op == 0) {
-                res = res > target ? res : target;
+                if (res < target) res = target, *next_move = depth == 0 ? i : *next_move;
                 if (res >= fa) return res;
             } else {
-                res = res < target ? res : target;
+                if (res > target) res = target, *next_move = depth == 0 ? i : *next_move;
                 if (res <= fa) return res;
             }
             memcpy(status, status_cpy, sizeof(status_cpy));
             continue;
         }
-        int nextFlag = (pos == 6 && flag == 1 || pos == 13 && flag == 2) ? flag : 3 - flag;
         if (op == 1) {
-            double t = dfs(next_move, nextFlag, 0, status, depth + 1, init_score1, init_score2, res);
+            double t = dfs(next_move, next_flag, flag == next_flag ? 1 : 0, status, depth + 1, init_score1, init_score2, res);
             memcpy(status, status_cpy, sizeof(status_cpy));
             if (res > t) res = t, *next_move = depth == 0 ? i : *next_move;
             if (res <= fa) break;
         } else {
-            double t = dfs(next_move, nextFlag, 1, status, depth + 1, init_score1, init_score2, res);
+            double t = dfs(next_move, next_flag, flag == next_flag ? 0 : 1, status, depth + 1, init_score1, init_score2, res);
             memcpy(status, status_cpy, sizeof(status_cpy));
             if (res < t) res = t, *next_move = depth == 0 ? i : *next_move;
             if (res >= fa) break;
