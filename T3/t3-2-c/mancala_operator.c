1ef1ecadce406ca0bcde93b8f4838afd5cb47d44
CajZella <cajzella@gmail.com>
2024-04-05 21:58:31 +0800

    t3-2 debug

--- T3/t3-2-c/mancala_operator.c
+++ T3/t3-2-c/mancala_operator.c
@@ -7,13 +7,11 @@
 #include <stdlib.h>
 
 
-double dfs(int* next_move_ptr, int flag, int op, int *status, int depth, int init_score1, int init_score2, double fa) { // flag: 1 for player 1, 2 for player 2
+double dfs(int* next_move, int flag, int op, int *status, int depth, int init_score1, int init_score2, double fa) { // flag: 1 for player 1, 2 for player 2
     // op = 0: max, op = 1: min
-    double weights[4] = {0.4, 0.3, 0.3};
-    int status_cpy[14];
+    double weights[4] = {0.5, 0.2, 0.5};
     int depths = 8;
-    (*next_move_ptr)++;
-    return *next_move_ptr;
+    int status_cpy[14];
     memcpy(status_cpy, status, sizeof(status_cpy));
     double res = op == 0 ? -1e9 : 1e9;
     for (int i = 0; i < 6; i++) {
@@ -25,15 +23,17 @@ double dfs(int* next_move_ptr, int flag, int op, int *status, int depth, int ini
             if (pos == 6 && flag == 2 || pos == 13 && flag == 1) continue;
             status[pos]++; stones--;
         }
-        if (depth + 1 == depths) {
+        if (status[pos] == 1 && status[12 - pos] > 0 && ((pos >= 1 && pos <= 6 && flag == 1) || (pos >= 8 && pos <= 13 && flag == 2)))
+            status[flag == 1 ? 6 : 13] += status[12 - pos] + 1, status[12 - pos] = status[pos] = 0;
+        int sum1 = 0, sum2 = 0, sum1_ = 0;
+        for (int j = 0; j < 6; j++) {
+            sum1 += status[j];
+            sum2 += status[j + 7];
+            sum1_ += status_cpy[j];
+        }
+        if (depth + 1 == depths || sum1 == 0 || sum2 == 0) {
             // calculate the features
             double features[4];
-            int sum1 = 0, sum2 = 0, sum1_ = 0;
-            for (int j = 0; j <= 6; j++) {
-                sum1 += status[j];
-                sum2 += status[j + 7];
-                sum1_ += status_cpy[j];
-            }
             features[0] = status[6] - init_score1 - (status[13] - init_score2);
             features[1] = sum1 - sum2;
             features[2] = sum1_ - sum1 + status[6] - init_score1;
@@ -55,28 +55,23 @@ double dfs(int* next_move_ptr, int flag, int op, int *status, int depth, int ini
         }
         int nextFlag = (pos == 6 && flag == 1 || pos == 13 && flag == 2) ? flag : 3 - flag;
         if (op == 1) {
-            double t = dfs(next_move_ptr,nextFlag, 0, status, depth + 1, init_score1, init_score2, res);
+            double t = dfs(next_move, nextFlag, 0, status, depth + 1, init_score1, init_score2, res);
             memcpy(status, status_cpy, sizeof(status_cpy));
-            if (res > t) res = t, *next_move_ptr = i;
+            if (res > t) res = t, *next_move = depth == 0 ? i : *next_move;
             if (res <= fa) break;
         } else {
-            double t = dfs(next_move_ptr, nextFlag, 1, status, depth + 1, init_score1, init_score2, res);
+            double t = dfs(next_move, nextFlag, 1, status, depth + 1, init_score1, init_score2, res);
             memcpy(status, status_cpy, sizeof(status_cpy));
-            if (res < t) res = t, *next_move_ptr = i;
+            if (res < t) res = t, *next_move = depth == 0 ? i : *next_move;
             if (res >= fa) break;
         }
     }
     return res;
 }
-
 int mancala_operator(int flag, int *status) {
-    int* next_move = malloc(sizeof(int));
-    *next_move = 100;
-    dfs(next_move ,flag, flag == 1 ? 0 : 1, status, 0, status[6], status[13], flag == 1 ? -1e9 : 1e9);
-    return flag * 10 + (*next_move) + 1;
-    /*int next_move = 0;
-    dfs(&next_move ,flag, flag == 1 ? 0 : 1, status, 0, status[6], status[13], flag == 1 ? -1e9 : 1e9);
-    return flag * 10 + (next_move) + 1;*/
+    int next_move = 0;
+    dfs(&next_move, flag, flag == 1 ? 0 : 1, status, 0, status[6], status[13], flag == 1 ? 1e9 : -1e9);
+    return flag * 10 + next_move + 1;
 }
 
 void *self_malloc(int len)
